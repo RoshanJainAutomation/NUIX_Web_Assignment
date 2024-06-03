86febdd2745bf57fc7fe72c8b1b407889b34ed7b
Roshan Jain <>
2024-06-03 16:23:47 +0530

    Removed Unused data

--- src/test/java/calculator/testcomponent/BaseTest.java
+++ src/test/java/calculator/testcomponent/BaseTest.java
@@ -1,14 +1,10 @@
 package calculator.testcomponent;
 
 import calculator.pages.CalculatorPage;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
